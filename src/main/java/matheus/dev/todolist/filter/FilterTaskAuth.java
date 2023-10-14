072a6ea13b5b6bcb003bd9215463f709c278a2b2
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-10-13 22:47:09 -0300

    Commit

--- src/main/java/matheus/dev/todolist/filter/FilterTaskAuth.java
+++ src/main/java/matheus/dev/todolist/filter/FilterTaskAuth.java
@@ -25,6 +25,10 @@ public class FilterTaskAuth extends OncePerRequestFilter{
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
         
+        var servletPath = request.getServletPath();
+
+        if(servletPath.startsWith("/tasks/")) {
+
         var authorization = request.getHeader("Authorization");
 
         var authEncoded = authorization.substring("Basic".length()).trim();
@@ -33,30 +37,29 @@ public class FilterTaskAuth extends OncePerRequestFilter{
 
         var authString = new String(authDecode);
 
-        String[] credential = authString.split(":");
-        String username = credential[0];
-        String password = credential[1];
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
 
         var user = this.userRepository.findByUsername(username);
         if(user == null) {
           response.sendError(401);
         }else {
-
           var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
           if(passwordVerify.verified) {
+            request.setAttribute("idUser", user.getId());
             filterChain.doFilter(request, response);
           }else {
             response.sendError(401);
           }
 
-          
         }
+     }else {
+      filterChain.doFilter(request, response);
+     }
 
-        filterChain.doFilter(request, response);
-  }
-
- 
+ }
 
 }
   
